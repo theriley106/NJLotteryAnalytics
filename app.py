84964b45c490274033b1893cfe1837c694940360
Theriley106 <christopherlambert106@gmail.com>
2019-03-20 17:01:22 -0700

    Added lottery simulator api

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
 import json
+import random
+import time
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -7,9 +9,24 @@ db = {}
 for val in json.load(open("data.json"))['games']:
 	db[val['gameId']] = val
 
-@app.route('/', methods=['GET'])
-def index():
-	return render_template("index.html")
+def play_ticket(gameId):
+	lotteryNum = random.randint(1, db[gameId]['totalTicketsPrinted'])
+	db[gameId]['totalTicketsPrinted'] = db[gameId]['totalTicketsPrinted'] - 1
+	response = {"success": True, "winning": False, "info": None, "number": lotteryNum, "time": int(time.time()), "ticketsLeft": db[gameId]['totalTicketsPrinted']}
+	prevNum = 0
+	for ticketTypes in db[gameId]['prizeTiers']:
+		if lotteryNum > prevNum and lotteryNum < (prevNum + ticketTypes['paidTickets']):
+			response['winning'] = True
+			response['info'] = ticketTypes
+			ticketTypes['paidTickets']  = ticketTypes['paidTickets'] - 1
+			break
+		prevNum += ticketTypes['paidTickets']
+	return response
+
+
+@app.route('/api/<gameId>', methods=['GET'])
+def index(gameId):
+	return jsonify(play_ticket(gameId))
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8000)
+	app.run(host='0.0.0.0', port=8000, debug=True)
